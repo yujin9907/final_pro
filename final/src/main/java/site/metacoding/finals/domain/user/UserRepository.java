1e9210a874e71551aea84f4caae6def378696495
yujin <heoyujin07@gmail.com>
2022-11-17 10:24:16 +0900

    세팅 domain 확인 완료

---
+++ final/src/main/java/site/metacoding/finals/domain/user/UserRepository.java
@@ -0,0 +1,10 @@
+package site.metacoding.finals.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    // 반환타입 메서드명 (매개변수)
+
+    // findBy규칙 : 문법
+    public User findByUsername(String username);
+}

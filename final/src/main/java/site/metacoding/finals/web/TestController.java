e00476c27666a0cdaae2ebfb86b22356bd861b8f
yujin <heoyujin07@gmail.com>
2022-12-02 19:53:44 +0900

    마무리

--- final/src/main/java/site/metacoding/finals/web/TestController.java
+++ final/src/main/java/site/metacoding/finals/web/TestController.java
@@ -5,7 +5,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
